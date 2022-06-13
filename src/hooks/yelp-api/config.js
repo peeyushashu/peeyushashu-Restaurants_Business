748aa11ee81bb9cdcf4c2c1326937bb421f8ae3f
Peeyush Goyal <34863102+peeyushashu@users.noreply.github.com>
2022-06-13 10:38:15 +0530

    Delete src directory

--- src/hooks/yelp-api/config.js
+++
@@ -1,3 +0,0 @@
-export const API_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3';
-
-export const BEARER_TOKEN = 'OciDqzxQi8K6WGWoI9Rs8X6GS5JcACQwpIn6xyg4MQf7c4X3KUvR5Gs17iZjMp_iGhxc6pdTukttzNYGfa-Teh6gRzqQW83Vxj3TRY7-7AcrrQoFOQMEfIRuPB6XYnYx';
